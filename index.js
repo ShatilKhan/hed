6abd871daf95bddfc1c821a189212b84f53b22b5
ShatilKhan <shatilshahriar009@gmail.com>
2024-06-25 12:47:36 +0600

    environment setup
    works with one warning
    WARNING: Consider using fromStringECDSA() or fromStringED2551() on a HEX-encoded
    string and fromStringDer() on a HEX-encoded string with DER prefix instead.

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
 require('dotenv').config();
-const express = require('express');
-const port = process.env.PORT || 4000;  // Read from .env if not available then defaults to 4000
-const accountId = process.env.ACCOUNT_ID;
-const privateKey = process.env.PRIVATE_KEY;
 
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-  console.log(accountId, privateKey);
-});
 
 async function environmentSetup() {
+    // Grad Account Id & Private Key from .env file
     const accountId = process.env.ACCOUNT_ID;
     const privateKey = process.env.PRIVATE_KEY;
 
-    console.log(accountId, privateKey);
+     // If not able to grab credentials, throw error
+     if(!accountId || !privateKey) {
+        throw new Error("Envrionment vars accountId & privateKey must be present!")
+    }
+
+    // Create Hedera testnet client
+    const client = Client.forTestnet();
+
+    // Set my account as the client's operator
+    client.setOperator(accountId, privateKey );
+
+    // Set default maxx trx fee in hbar
+    client.setDefaultMaxTransactionFee(new Hbar(100));
+
+    // Set Max payment for queries in hbar
+    client.setDefaultMaxQueryPayment(new Hbar(50));
+
+   
 }
+
+environmentSetup();
+
+
 
