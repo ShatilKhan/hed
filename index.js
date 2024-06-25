294815b3fbf51b0b6ad1460e21440a013e4e9165
ShatilKhan <shatilshahriar009@gmail.com>
2024-06-26 00:34:39 +0600

    Create New Account & Check Balance
    Created a new Hedera account with an initial balance of 1,000 tinybars.
    
    Obtained the new account ID by requesting the receipt of the transaction.
    
    Verified the starting balance of the new account by submitting a query to the network.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
+const { Client, 
+    PrivateKey, 
+    AccountCreateTransaction, 
+    AccountBalanceQuery,
+    Hbar, 
+    TransferTransaction } = require("@hashgraph/sdk");
 require('dotenv').config();
 
 
 async function environmentSetup() {
     // Grad Account Id & Private Key from .env file
-    const accountId = process.env.ACCOUNT_ID;
-    const privateKey = process.env.PRIVATE_KEY;
+    const myAccountId = process.env.ACCOUNT_ID;
+    const myPrivateKey = PrivateKey.fromStringECDSA(process.env.PRIVATE_KEY);
 
      // If not able to grab credentials, throw error
-     if(!accountId || !privateKey) {
+     if(!myAccountId || !myPrivateKey) {
         throw new Error("Envrionment vars accountId & privateKey must be present!")
     }
 
@@ -16,7 +21,7 @@ async function environmentSetup() {
     const client = Client.forTestnet();
 
     // Set my account as the client's operator
-    client.setOperator(accountId, privateKey );
+    client.setOperator(myAccountId, myPrivateKey );
 
     // Set default maxx trx fee in hbar
     client.setDefaultMaxTransactionFee(new Hbar(100));
@@ -24,6 +29,30 @@ async function environmentSetup() {
     // Set Max payment for queries in hbar
     client.setDefaultMaxQueryPayment(new Hbar(50));
 
+    // Create new keys
+    const newAccountPrivateKey = PrivateKey.generateED25519();
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    // Create new account with 1000 tinybars starting balance
+    const newAccount = await new AccountCreateTransaction()
+        .setKey(newAccountPublicKey)
+        .setInitialBalance(Hbar.fromTinybars(1000))
+        .execute(client);
+
+    // Get the new account ID
+    const getReceipt = await newAccount.getReceipt(client);
+    const newAccountId = getReceipt.accountId;
+
+    // Log the account ID
+    console.log("The new account ID is : " +newAccountId);
+
+    // Verify the account balance
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    // Log the account balance
+    console.log("The new account balance is : " +accountBalance.hbars.toTinybars() + " tinybar.")
    
 }
 
