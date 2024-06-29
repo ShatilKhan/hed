ac1192087a784eb66798a7380b6d12359553cbe6
ShatilKhan <shatilshahriar009@gmail.com>
2024-06-29 21:29:17 +0600

    Simple Smart Contract in Solidity

---
+++ simplesc.sol
@@ -0,0 +1,16 @@
+// Simple Smart Contract Example
+// SPDX-License-identifier: MIT
+pragma solidity >= 0.4.22 < 0.9.0;
+
+contract SimpleStorage {
+    uint256 data;
+
+    function retrieve() external view returns(uint256) {
+        return data;
+    }
+
+    function store(uint256 _data) external{
+        data = _data;
+    }
+
+}
